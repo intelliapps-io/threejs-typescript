fc0a532c2986055cac4e65fac6fb76735a412975
Intelliapps <41976910+intelliapps-io@users.noreply.github.com>
2020-02-11 22:35:28 -0500

    added text

--- intelliapps-landing/src/index.ts
+++ intelliapps-landing/src/index.ts
@@ -2,7 +2,7 @@ import * as THREE from 'three'
 import { initializeEnviorment } from './helpers/initializeEnviorment';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { jsonfont } from './helpers/jsonfont'
-
+import { Text } from './lib/Text'
 
 const { scene, camera, renderer } = initializeEnviorment()
 camera.position.set(20, 300, 400)
@@ -16,89 +16,101 @@ scene.add(light);
 
 //////// LINES
 
-var textMesh1, textMesh2, textGeo, textGeo;
-const materials = [
-  new THREE.MeshPhongMaterial( { color: 0xffffff, flatShading: true } ), // front
-  new THREE.MeshPhongMaterial( { color: 0xffffff } ) // side
-]
+// var textMesh1, textMesh2, textGeo, textGeo;
+// const materials = [
+//   new THREE.MeshPhongMaterial( { color: 0xffffff, flatShading: true } ), // front
+//   new THREE.MeshPhongMaterial( { color: 0xffffff } ) // side
+// ]
 
-const group = new THREE.Group()
+// const group = new THREE.Group()
 
-function createText() {
-  const font = new THREE.Font(jsonfont),
-    height = 20,
-    size = 70,
-    curveSegments = 4
+// function createText() {
+//   const font = new THREE.Font(jsonfont),
+//     height = 20,
+//     size = 70,
+//     curveSegments = 4
 
-  textGeo = new THREE.TextGeometry("INTELLIAPPS.IO", {
-    font,
-    size,
-    height,
-    curveSegments
-  });
+//   textGeo = new THREE.TextGeometry("INTELLIAPPS.IO", {
+//     font,
+//     size,
+//     height,
+//     curveSegments
+//   });
 
-  textGeo.computeBoundingBox();
-  textGeo.computeVertexNormals();
+//   textGeo.computeBoundingBox();
+//   textGeo.computeVertexNormals();
 
-  var triangle = new THREE.Triangle();
+//   var triangle = new THREE.Triangle();
 
-  // "fix" side normals by removing z-component of normals for side faces
-  // (this doesn't work well for beveled geometry as then we lose nice curvature around z-axis)
+//   // "fix" side normals by removing z-component of normals for side faces
+//   // (this doesn't work well for beveled geometry as then we lose nice curvature around z-axis)
 
-  var triangleAreaHeuristics = 0.1 * (height * size);
+//   var triangleAreaHeuristics = 0.1 * (height * size);
 
-  for (var i = 0; i < textGeo.faces.length; i++) {
+//   for (var i = 0; i < textGeo.faces.length; i++) {
 
-    var face = textGeo.faces[i];
+//     var face = textGeo.faces[i];
 
-    if (face.materialIndex == 1) {
+//     if (face.materialIndex == 1) {
 
-      for (var j = 0; j < face.vertexNormals.length; j++) {
+//       for (var j = 0; j < face.vertexNormals.length; j++) {
 
-        face.vertexNormals[j].z = 0;
-        face.vertexNormals[j].normalize();
+//         face.vertexNormals[j].z = 0;
+//         face.vertexNormals[j].normalize();
 
-      }
+//       }
 
-      var va = textGeo.vertices[face.a];
-      var vb = textGeo.vertices[face.b];
-      var vc = textGeo.vertices[face.c];
+//       var va = textGeo.vertices[face.a];
+//       var vb = textGeo.vertices[face.b];
+//       var vc = textGeo.vertices[face.c];
 
-      var s = triangle.set(va, vb, vc).getArea();
+//       var s = triangle.set(va, vb, vc).getArea();
 
-      if (s > triangleAreaHeuristics) {
+//       if (s > triangleAreaHeuristics) {
 
-        for (var j = 0; j < face.vertexNormals.length; j++) {
+//         for (var j = 0; j < face.vertexNormals.length; j++) {
 
-          face.vertexNormals[j].copy(face.normal);
+//           face.vertexNormals[j].copy(face.normal);
 
-        }
+//         }
 
-      }
+//       }
 
-    }
+//     }
 
-  }
+//   }
 
-  var centerOffset = - 0.5 * (textGeo.boundingBox.max.x - textGeo.boundingBox.min.x);
+//   var centerOffset = - 0.5 * (textGeo.boundingBox.max.x - textGeo.boundingBox.min.x);
 
-  textGeo = new THREE.BufferGeometry().fromGeometry(textGeo);
+//   textGeo = new THREE.BufferGeometry().fromGeometry(textGeo);
 
-  textMesh1 = new THREE.Mesh(textGeo, materials);
+//   textMesh1 = new THREE.Mesh(textGeo, materials);
 
-  textMesh1.position.x = centerOffset;
-  // textMesh1.position.y = hover;
-  textMesh1.position.z = 0;
+//   textMesh1.position.x = centerOffset;
+//   // textMesh1.position.y = hover;
+//   textMesh1.position.z = 0;
 
-  textMesh1.rotation.x = 0;
-  // textMesh1.rotation.y = Math.PI * 2;
+//   textMesh1.rotation.x = 0;
+//   // textMesh1.rotation.y = Math.PI * 2;
 
-  group.add(textMesh1);
-}
+//   group.add(textMesh1);
+// }
 
-createText()
+// createText()
 
-scene.add( group );
+// scene.add( group );
+
+const text = new Text('INTELLIAPPS.IO', {
+  curveSegments: 10,
+  hight: 10,
+  size: 70
+})
+
+scene.add(text.getGroup())
+
+text.getShapes().forEach(shape => {
+  scene.add(shape)
+})
 
 //////////////////
 // Drawing Loop //
---
+++ intelliapps-landing/src/lib/Text.ts
@@ -0,0 +1,92 @@
+import * as THREE from 'three'
+import { jsonfont } from '../helpers/jsonfont'
+
+
+export interface ITextOptions {
+  hight: number
+  size: number
+  curveSegments: number
+}
+
+export class Text {
+  text: string = ''
+  options: ITextOptions
+  textGeometry = new THREE.TextGeometry(this.text, { font: new THREE.Font(jsonfont) })
+  shapeGeometry = new THREE.BufferGeometry()
+  textMesh = new THREE.Mesh()
+  group = new THREE.Group()
+  shapes: THREE.Mesh[] = []
+
+  constructor(text: string, options: ITextOptions) {
+    this.options = options
+    this.setText(text)
+  }
+
+  public setText(text: string) {
+    this.text = text
+    this.shapes = []
+    this.textGeometry = new THREE.TextGeometry(this.text, { ...this.options, font: new THREE.Font(jsonfont) })
+    this.textGeometry.computeBoundingBox()
+    this.textGeometry.computeVertexNormals()
+
+    const triangle = new THREE.Triangle(),
+      triangleArea = 0.1 * this.options.hight * this.options.size
+
+
+    this.textGeometry.faces.forEach(face => {
+      var va = this.textGeometry.vertices[face.a];
+      var vb = this.textGeometry.vertices[face.b];
+      var vc = this.textGeometry.vertices[face.c];
+
+      if (face.materialIndex == 1) {
+        for (var j = 0; j < face.vertexNormals.length; j++) {
+          face.vertexNormals[j].z = 0;
+          face.vertexNormals[j].normalize();
+        }
+
+        var s = triangle.set(va, vb, vc).getArea();
+
+        if (s > triangleArea) {
+          for (var j = 0; j < face.vertexNormals.length; j++) {
+            face.vertexNormals[j].copy(face.normal);
+          }
+        }
+      }
+
+      // CREATE SHAPES
+      const triGeo = new THREE.BufferGeometry()
+      const triMat = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+      let vertices = new Float32Array([
+        va.x, va.y, va.z,
+        vb.x, vb.y, vb.z,
+        vc.x, vc.y, vc.z,
+      ])
+      triGeo.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+      this.shapes.push(new THREE.Mesh(triGeo, triMat))
+    })
+
+    const materials = [
+      new THREE.MeshPhongMaterial({ color: 0xffffff, flatShading: true }), // front
+      new THREE.MeshPhongMaterial({ color: 0xffffff }) // side
+    ]
+
+    var centerOffset = - 0.5 * (this.textGeometry.boundingBox.max.x - this.textGeometry.boundingBox.min.x);
+
+    this.shapeGeometry = new THREE.BufferGeometry().fromGeometry(this.textGeometry);
+
+    this.textMesh = new THREE.Mesh(this.shapeGeometry, materials);
+
+    this.textMesh.position.x = centerOffset;
+    // textMesh1.position.y = hover;
+    this.textMesh.position.z = 0;
+
+    this.textMesh.rotation.x = 0;
+    // textMesh1.rotation.y = Math.PI * 2;
+
+    this.group.add(this.textMesh);
+  }
+
+  public getGroup = () => this.group
+
+  public getShapes = () => this.shapes
+}
